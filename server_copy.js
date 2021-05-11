61de8b439d5c929a8d5045675753187083db9803
UniM0cha <77760474+UniM0cha@users.noreply.github.com>
2021-05-11 11:22:43 +0900

    dd

--- server_copy.js
+++ server_copy.js
@@ -1,19 +1,15 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
 
-const server = http.createServer((reqest, response) => {
-    fs.readFile('./index.html', (err,data) => {
-        if (err){
-            throw err;
-        }
-        response.end(data);
-    })
+app.set('port', 3001);
+app.set('view engine', 'html');
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+}, (req, res) => {
+    throw new Error('에러다 이놈아');
 });
 
-server.listen(3000);
-server.on('listening', () => {
-    console.log('3000번 포트에서 서버 대기중!');
-});
-server.on('error', (error) => {
-    console.error(error);
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
 });
