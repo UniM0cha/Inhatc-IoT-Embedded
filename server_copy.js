6f041477037357109306cdf1510e8da53d9ad489
UniM0cha <77760474+UniM0cha@users.noreply.github.com>
2021-05-11 11:52:48 +0900

    오 해결했어...

--- server_copy.js
+++ server_copy.js
@@ -2,13 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.set('port', 3001);
+app.set('port', 3000);
 app.set('view engine', 'html');
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
-}, (req, res) => {
-    throw new Error('에러다 이놈아');
-});
+// 정적으로 경로를 지정해주어서 그 안의 파일들을 모두 불러와주는 것.
+app.use('/', express.static(path.join(__dirname, 'view')));
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
