0192b2d35e2f0b64477ece35efaf1528caf714cb
UniM0cha <77760474+UniM0cha@users.noreply.github.com>
2021-06-01 21:10:17 +0900

    폴더 이동

---
+++ 예제/serialport_check.js
@@ -0,0 +1,8 @@
+const SerialPort = require('serialport');
+SerialPort.list().then(ports => {
+  ports.forEach(function(port) {
+    console.log(port.path);
+    console.log(port.pnpId);
+    console.log(port.manufacturer);
+  });
+});
