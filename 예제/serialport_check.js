52fa9e646275fcdf5e54340d66346e2d72f3bc99
UniM0cha <solst_ice@naver.com>
2022-09-20 18:39:15 +0900

    0920 - 파일 정리

--- 예제/serialport_check.js
+++
@@ -1,8 +0,0 @@
-const SerialPort = require('serialport');
-SerialPort.list().then(ports => {
-  ports.forEach(function(port) {
-    console.log(port.path);
-    console.log(port.pnpId);
-    console.log(port.manufacturer);
-  });
-});
